953c1b4a2050ef02cd08051868e4ee6de57100de
tonguiino <santiagotq2@gmail.com>
2026-06-17 08:05:35 -0500

    Avance codigo login y js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $("#togglePassword").on("click", function () {
+    const passwordInp = $("#password");
+    const isPassword = passwordInp.attr("type") === "password";
 
-
-
-    
-})
+    if (isPassword) {
+      passwordInp.attr("type", "text");
+      $("#eyeOpen").addClass("d-none");
+      $("#eyeOff").removeClass("d-none");
+    } else {
+      passwordInp.attr("type", "password");
+      $("#eyeOff").addClass("d-none");
+      $("#eyeOpen").removeClass("d-none");
+    }
+  });
+});
